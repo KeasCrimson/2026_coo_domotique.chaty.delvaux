0ecc24028aeb44e90301b1982c3a7f0dedfb26ac
DELVAUX Thomas <thomas.delvaux@orange.fr>
2026-04-09 16:42:28 +0200

    Chagement de la class Telecommande.java pour implémanter l'interface Périphérique

--- TD04_delvaux_thomas/src/Telecommande.java
+++ TD04_delvaux_thomas/src/Telecommande.java
@@ -1,47 +1,34 @@
 import java.util.ArrayList;
 
 public class Telecommande {
-    public ArrayList<Lampe> lampes;
-    private ArrayList<Hifi> hifis;
+    public ArrayList<Peripherique> peripherie;
     public Telecommande() {
-        lampes = new ArrayList<Lampe>();
-        hifis = new ArrayList<Hifi>();
+        peripherie = new ArrayList<Peripherique>();
     }
-    public void ajouterLampe(Lampe l) {
-        lampes.add(l);
-    }
-    public void ajouterHifi(Hifi f) {
-        hifis.add(f);
+    public void activerPeripherique(Peripherique p) {
+        peripherie.add(p);
     }
 
-    public boolean activerLampe(int n) {
+    public boolean allumerPeripherique(int n) {
         boolean activation = false;
-        if (0 <= n && n < lampes.size()) {
-            lampes.get(n).allumer();
+        if (0 <= n && n < peripherie.size()) {
+            peripherie.get(n).allumer();
             activation = true;
         }
         return activation;
     }
-    public boolean activerHifi(int n) {
-        boolean activation = false;
-        if (0 <= n && n < hifis.size()) {
-            hifis.get(n).allumer();
-            activation = true;
-        }
-        return activation;
-    }
-    public boolean desactiverLampe(int n) {
+    public boolean eteindrePeripherique(int n) {
         boolean desactiver = false;
-        if (0 <= n && n < lampes.size()) {
-            lampes.get(n).eteindre();
+        if (0 <= n && n < peripherie.size()) {
+            peripherie.get(n).eteindre();
                 desactiver = true;
         }
         return desactiver;
     }
-    public boolean desactiverHifi(int n) {
+    public boolean desactiverPeripherique(int n) {
         boolean activation = false;
-        if (0 <= n && n < hifis.size()) {
-            hifis.get(n).eteindre();
+        if (0 <= n && n < peripherie.size()) {
+            peripherie.get(n).remove();
             activation = true;
         }
         return activation;
@@ -49,20 +36,16 @@ public class Telecommande {
     public boolean activerTout() {
         int i = 0;
         boolean activer = false;
-        while (i < lampes.size() && i < hifis.size()) {
-            lampes.get(i).allumer();
-            hifis.get(i).allumer();
+        while (i < peripherie.size() && i < peripherie.size()) {
+            peripherie.get(i).allumer();
             i++;
         }
         return activer;
     }
     public String toString() {
         String resultat = "";
-        for (int i = 0; i < lampes.size(); i++) {
-            resultat += lampes.get(i).toString()+"\n";
-        }
-        for (int i = 0; i < hifis.size(); i++) {
-            resultat += hifis.toString()+"\n";
+        for (int i = 0; i < peripherie.size(); i++) {
+            resultat += peripherie.get(i).toString()+"\n";
         }
         return resultat;
     }
