907bdb0fc81ccdb6086deb1981a0039638649f0e
Aloys Chaty <aloys.chaty9@etu.univ-lorraine.fr>
2026-04-09 14:43:01 +0200

    Diagramme UML du TD5

--- Projet_chaty/src/Telecommande.java
+++ Projet_chaty/src/Telecommande.java
@@ -2,11 +2,16 @@ import java.util.ArrayList;
 
 public class Telecommande {
     public ArrayList<Lampe> lampes;
+    private ArrayList<Hifi> hifis;
     public Telecommande() {
         lampes = new ArrayList<Lampe>();
+        hifis = new ArrayList<Hifi>();
     }
     public void ajouterLampe(Lampe l) {
         lampes.add(l);
+    }
+    public void ajouterHifi(Hifi f) {
+        hifis.add(f);
     }
 
     public boolean activerLampe(int n) {
@@ -17,19 +22,36 @@ public class Telecommande {
         }
         return activation;
     }
+    public boolean activerHifi(int n) {
+        boolean activation = false;
+        if (0 <= n && n < hifis.size()) {
+            hifis.get(n).allumer();
+            activation = true;
+        }
+        return activation;
+    }
     public boolean desactiverLampe(int n) {
         boolean desactiver = false;
         if (0 <= n && n < lampes.size()) {
-                lampes.get(n).eteindre();
+            lampes.get(n).eteindre();
                 desactiver = true;
         }
         return desactiver;
     }
+    public boolean desactiverHifi(int n) {
+        boolean activation = false;
+        if (0 <= n && n < hifis.size()) {
+            hifis.get(n).eteindre();
+            activation = true;
+        }
+        return activation;
+    }
     public boolean activerTout() {
         int i = 0;
         boolean activer = false;
-        while (i < lampes.size()) {
+        while (i < lampes.size() && i < hifis.size()) {
             lampes.get(i).allumer();
+            hifis.get(i).allumer();
             i++;
         }
         return activer;
@@ -37,7 +59,10 @@ public class Telecommande {
     public String toString() {
         String resultat = "";
         for (int i = 0; i < lampes.size(); i++) {
-            resultat += lampes.get(i).toString();
+            resultat += lampes.get(i).toString()+"\n";
+        }
+        for (int i = 0; i < hifis.size(); i++) {
+            resultat += hifis.toString()+"\n";
         }
         return resultat;
     }
