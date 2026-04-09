fd18e375a44a59071b83c21c5df5dde84aa52a90
DELVAUX Thomas <thomas.delvaux@orange.fr>
2026-04-09 17:53:04 +0200

    Ajout du planteuml et de peripherique.java

--- TD04_delvaux_thomas/src/Peripherique.java
+++ TD04_delvaux_thomas/src/Peripherique.java
@@ -2,10 +2,10 @@ public interface Peripherique {
     /**
      * permet de changer l'etat interne d'un périphérique en l'allumant
      */
-    public void allumer(){}
+    public void allumer();
 
     /**
      * permet de changer l'etat interne d'une lampe en l'eteignant
      */
-    public void eteindre() {}
+    public void eteindre();
 }
