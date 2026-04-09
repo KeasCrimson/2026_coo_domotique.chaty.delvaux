245dbbfb8ede67dda18468129677aeb85410d937
DELVAUX Thomas <thomas.delvaux@orange.fr>
2026-04-09 16:49:50 +0200

    Modification de la class Peripherique.java

--- TD04_delvaux_thomas/src/Peripherique.java
+++ TD04_delvaux_thomas/src/Peripherique.java
@@ -1,28 +1,11 @@
-public class Peripherique {
+public interface Peripherique {
     /**
      * permet de changer l'etat interne d'un périphérique en l'allumant
      */
-    public void allumer() {
-        this.allume = true;
-    }
+    public void allumer(){}
 
     /**
      * permet de changer l'etat interne d'une lampe en l'eteignant
      */
-    public void eteindre() {
-        this.allume = false;
-    }
-
-    /**
-     * retourne le descriptif de la lampe sous la forme nom + "On"/"Off"
-     */
-    public String toString() {
-        String r = "";
-        if (this.allume) {
-            r = "On";
-        } else {
-            r = "Off";
-        }
-        return (nom + ": " + r);
-    }
+    public void eteindre() {}
 }
