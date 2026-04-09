1a035271d78f9ff32692ac666ea0c77e1d5a51a6
Aloys Chaty <aloys.chaty9@etu.univ-lorraine.fr>
2026-04-09 07:57:40 +0200

    Correction de Télécommande et update du diagramme de classe

--- TD04_delvaux_thomas/src/Telecommande.java
+++ TD04_delvaux_thomas/src/Telecommande.java
@@ -1,47 +1,47 @@
 import java.util.ArrayList;
 
 public class Telecommande {
-    public ArrayList<Lampe> telecommande;
-    private ArrayList<Hifi> telecommandehifi;
+    public ArrayList<Lampe> lampes;
+    private ArrayList<Hifi> hifis;
     public Telecommande() {
-        telecommande = new ArrayList<Lampe>();
-        telecommandehifi = new ArrayList<Hifi>();
+        lampes = new ArrayList<Lampe>();
+        hifis = new ArrayList<Hifi>();
     }
     public void ajouterLampe(Lampe l) {
-        telecommande.add(l);
+        lampes.add(l);
     }
     public void ajouterHifi(Hifi f) {
-        telecommandehifi.add(f);
+        hifis.add(f);
     }
 
     public boolean activerLampe(int n) {
         boolean activation = false;
-        if (0 <= n && n < telecommande.size()) {
-            telecommande.get(n).allumer();
+        if (0 <= n && n < lampes.size()) {
+            lampes.get(n).allumer();
             activation = true;
         }
         return activation;
     }
     public boolean activerHifi(int n) {
         boolean activation = false;
-        if (0 <= n && n < telecommandehifi.size()) {
-            telecommandehifi.get(n).allumer();
+        if (0 <= n && n < hifis.size()) {
+            hifis.get(n).allumer();
             activation = true;
         }
         return activation;
     }
     public boolean desactiverLampe(int n) {
         boolean desactiver = false;
-        if (0 <= n && n < telecommande.size()) {
-            telecommande.get(n).eteindre();
+        if (0 <= n && n < lampes.size()) {
+            lampes.get(n).eteindre();
                 desactiver = true;
         }
         return desactiver;
     }
     public boolean desactiverHifi(int n) {
         boolean activation = false;
-        if (0 <= n && n < telecommandehifi.size()) {
-            telecommandehifi.get(n).eteindre();
+        if (0 <= n && n < hifis.size()) {
+            hifis.get(n).eteindre();
             activation = true;
         }
         return activation;
@@ -49,20 +49,20 @@ public class Telecommande {
     public boolean activerTout() {
         int i = 0;
         boolean activer = false;
-        while (i < telecommande.size() && i < telecommandehifi.size()) {
-            telecommande.get(i).allumer();
-            telecommandehifi.get(i).allumer();
+        while (i < lampes.size() && i < hifis.size()) {
+            lampes.get(i).allumer();
+            hifis.get(i).allumer();
             i++;
         }
         return activer;
     }
     public String toString() {
         String resultat = "";
-        for (int i = 0; i < telecommande.size(); i++) {
-            resultat += telecommande.get(i).toString()+"\n";
+        for (int i = 0; i < lampes.size(); i++) {
+            resultat += lampes.get(i).toString()+"\n";
         }
-        for (int i = 0; i < telecommandehifi.size(); i++) {
-            resultat += telecommandehifi.toString()+"\n";
+        for (int i = 0; i < hifis.size(); i++) {
+            resultat += hifis.toString()+"\n";
         }
         return resultat;
     }
