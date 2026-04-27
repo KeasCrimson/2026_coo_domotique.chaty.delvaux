186389f43feed1299b3126a57cddfd2b2577777c
Aloys Chaty <aloys.chaty9@etu.univ-lorraine.fr>
2026-04-27 14:57:53 +0200

    Ajout des tests de la nouvelle classe Telecommande

--- Projet_Telecommande/test/TelecommandeTest.java
+++ Projet_Telecommande/test/TelecommandeTest.java
@@ -4,74 +4,101 @@ import static org.testng.AssertJUnit.assertEquals;
 
 public class TelecommandeTest {
 
+   @Test
+    public void test_AjouterAppareil(){
+       Telecommande t = new Telecommande();
+
+       //Création des appareils
+       Lampe l = new Lampe("Bob");
+       Television tel = new Television("Bobby");
+       Hifi h = new Hifi();
+       Cheminee c = new Cheminee();
+       AdapterCheminee ac = new AdapterCheminee(c);
+
+       //Ajout des appareils dans la télécommande
+       t.ajouterAppareil(l);
+       t.ajouterAppareil(tel);
+       t.ajouterAppareil(h);
+       t.ajouterAppareil(ac);
+
+       assertEquals(t.appareils.get(0), l);
+       assertEquals(t.appareils.get(1), tel);
+       assertEquals(t.appareils.get(2), h);
+       assertEquals(t.appareils.get(3), ac);
+
+   }
+
+   @Test
+    public void test_ActiverAppareil(){
+       Telecommande t = new Telecommande();
+
+       //Création des appareils
+       Lampe l = new Lampe("Bob");
+       Television tel = new Television("Bobby");
+       Hifi h = new Hifi();
+       Cheminee c = new Cheminee();
+       AdapterCheminee ac = new AdapterCheminee(c);
+
+       //Ajout des appareils dans la télécommande
+       t.ajouterAppareil(l);
+       t.ajouterAppareil(tel);
+       t.ajouterAppareil(h);
+       t.ajouterAppareil(ac);
+
+       boolean test0 = t.activerAppareil(0);
+       boolean test1 = t.activerAppareil(1);
+       boolean test2 = t.activerAppareil(2);
+       boolean test3 =t.activerAppareil(3);
+       boolean test4 =t.activerAppareil(4);
+
+       //Tests
+       assertEquals(test0, true);
+       assertEquals(test1, true);
+       assertEquals(test2, true);
+       assertEquals(test3, true);
+       assertEquals(test4, false);
+
+   }
+
+
     @Test
-    public void test_ajoutTelecommandeVide(){
+    public void test_EteindreAppareil() {
+        Telecommande t = new Telecommande();
+
+        //Création des appareils
         Lampe l = new Lampe("Bob");
-        Telecommande t = new Telecommande();
-        t.ajouterLampe(l);
+        Television tel = new Television("Bobby");
+        Hifi h = new Hifi();
+        Cheminee c = new Cheminee();
+        AdapterCheminee ac = new AdapterCheminee(c);
 
-        assertEquals(t.lampes.get(0), l);
+        //Ajout des appareils dans la télécommande
+        t.ajouterAppareil(l);
+        t.ajouterAppareil(tel);
+        t.ajouterAppareil(h);
+        t.ajouterAppareil(ac);
+
+        t.activerAppareil(0);
+        t.activerAppareil(1);
+        t.activerAppareil(2);
+        t.activerAppareil(3);
+        t.activerAppareil(4);
+
+        boolean test0 = t.desactiverAppareil(0);
+        boolean test1 = t.desactiverAppareil(1);
+        boolean test2 = t.desactiverAppareil(2);
+        boolean test3 = t.desactiverAppareil(3);
+        boolean test4 = t.desactiverAppareil(4);
+
+
+        //Tests
+        assertEquals(test0, true);
+        assertEquals(test1, true);
+        assertEquals(test2, true);
+        assertEquals(test3, true);
+        assertEquals(test4, false);
     }
 
-    @Test
-    public void test_ajouterTelecommande1Element(){
-
-        //Initialisation des valeurs
-        Lampe l1 = new Lampe("Bob");
-        Lampe l2 = new Lampe("Bobby");
-
-        Telecommande t = new Telecommande();
-
-        t.ajouterLampe(l1);
-
-        t.ajouterLampe(l2);
-
-        //Test
-        assertEquals(t.lampes.get(0), l1);
-        assertEquals(t.lampes.get(1), l2);
-
-    }
-
-    @Test
-    public void test_activationLampeZero(){
-        Lampe l1 = new Lampe("Bob");
-        Lampe l2 = new Lampe("Bobby");
-        Telecommande t = new Telecommande();
-        t.ajouterLampe(l1);
-        t.ajouterLampe(l2);
-
-        t.activerLampe(0);
-
-        assertEquals(true, l1.isAllume());
-    }
-
-    @Test
-    public void test_activationLampeUn(){
-        Lampe l1 = new Lampe("Bob");
-        Lampe l2 = new Lampe("Bobby");
-        Telecommande t = new Telecommande();
-        t.ajouterLampe(l1);
-        t.ajouterLampe(l2);
-
-        t.activerLampe(1);
-
-        assertEquals(false, l1.isAllume());
-        assertEquals(true, l2.isAllume());
-    }
-
-    @Test
-    public void test_activationLampeInexistante(){
-        Lampe l1 = new Lampe("Bob");
-        Lampe l2 = new Lampe("Bobby");
-        Telecommande t = new Telecommande();
-        t.ajouterLampe(l1);
-        t.ajouterLampe(l2);
-
-        t.activerLampe(2);
-
-        assertEquals(false, l1.isAllume());
-        assertEquals(false, l2.isAllume());
-    }
 
 
 }
