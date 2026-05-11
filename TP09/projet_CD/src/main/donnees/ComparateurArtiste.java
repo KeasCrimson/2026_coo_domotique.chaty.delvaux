0226a416b6aad17f31bceb02354d5657d27d3609
DELVAUX Thomas <thomas.delvaux@orange.fr>
2026-05-11 15:46:48 +0200

    Modification de la class ComparateurArtiste.java

--- TP09/projet_CD/src/main/donnees/ComparateurArtiste.java
+++ TP09/projet_CD/src/main/donnees/ComparateurArtiste.java
@@ -3,7 +3,7 @@ package donnees;
 public class ComparateurArtiste implements ComparateurCD {
     @Override
     public boolean etreAvant(CD cd1, CD cd2) {
-        if (cd1.compareTo(cd2) == -1) {
+        if (cd1.getNomArtiste().compareTo(cd2.getNomArtiste()) == -1) {
             return true;
         }
         return false;
